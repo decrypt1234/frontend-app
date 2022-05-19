e482158236cd2a0189c1f30755edc3f37ae9cc73
rachna-decrypt <rachna.decrypt@gmail.com>
2022-05-19 16:57:45 +0530

    apiService sWalletAddress to walletAddress

--- src/apiServices.js
+++ src/apiServices.js
@@ -123,7 +123,7 @@ export const checkuseraddress = async (account) => {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      sWalletAddress: account,
+      walletAddress: account,
     }),
   };
   try {
