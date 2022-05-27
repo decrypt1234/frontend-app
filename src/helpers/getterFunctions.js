fe0d3de4831a2faf27bef6350751664b726ae268
rachna-decrypt <rachna.decrypt@gmail.com>
2022-05-27 17:41:34 +0530

    counter added

--- src/helpers/getterFunctions.js
+++ src/helpers/getterFunctions.js
@@ -691,6 +691,7 @@ export const getUpcomingMints = async (req) => {
           name: coll.name,
           desc: coll.description,
           saleStartTime: coll.preSaleStartTime,
+          saleEndTime: coll.preSaleEndTime,
           price: coll.price.$numberDecimal,
           items: coll.nftCount
         };
